6442acbb5f0da1fe31a7d1663181b20baeefb434
Harsh0664 <shilpajain0664@gmail.com>
2023-08-26 23:04:27 +0530

    Updating word.js

--- javascript/word.js
+++ javascript/word.js
@@ -1,26 +1,27 @@
 var wordToGuessed = null
-async function generateWord() {
+export async function generateWord() {
     try {
         const response = await fetch('https://random-word-api.herokuapp.com/word?length=5')
         const data = await response.json()
         wordToGuessed = data[0]
-        getDetailsOfWord(wordToGuessed)
+        const details = getDetailsOfWord(wordToGuessed, true)
+        return details
     } catch (error) {
         console.log(error)
     }
 }
-generateWord()
 
-async function getDetailsOfWord(wordToGuessed) {
+export async function getDetailsOfWord(wordToGuessed, flag) {
     try {
         const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordToGuessed}`)
         const data = await response.json()
-        console.log("Word Generated: ", wordToGuessed)
-        console.log("Details of word: ")
-        console.log(data)
+        // console.log("Word Generated: ", wordToGuessed)
+        // console.log("Details of word: ")
+        // console.log(data)
+        return data
     } catch (error) {
-        console.log("Word not found")
-        generateWord()
+        // console.log("Word not found")
+        if(flag) generateWord()
     }
 }
 
